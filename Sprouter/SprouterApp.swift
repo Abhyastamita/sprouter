a8e660aff757bdf3ba198dbee5acf631401c45bc
Katherine Perdue <krp0047@auburn.edu>
2023-04-13 17:08:03 -0400

    Initial Commit

---
+++ Sprouter/SprouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprouterApp.swift
+//  Sprouter
+//
+//  Created by user232612 on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SprouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
